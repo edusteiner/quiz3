94e19a0c9cf8b94fac06b59b277e7a9132bd5335
edusteiner <edusteiner@gmail.com>
2017-01-03 22:55:04 -0200

    Add files via upload
    3a funcao

--- quiz3.R
+++ quiz3.R
@@ -111,7 +111,7 @@ rankhospital <- function(state,outcome,num = "best") {
         cat(paste("ESTADO =", state, "   OUTCOME =", outcome, "   RESPOSTA =", outcome_por_estado[num,2]  ,"\n")) 
           }
       
-      
+       
       
       #cat(paste("ESTADO =", state, "   OUTCOME =", outcome, "   RESPOSTA =", outcome_por_estado[num,2]  ,"\n"))
       
@@ -123,6 +123,74 @@ rankhospital <- function(state,outcome,num = "best") {
     
   }
 }
+
+
+
+
+
+
+
+rankall <- function(outcome, num = "best") {
+  setwd("/Users/eduardo.steiner/Documents/CursoDataScience/quiz3")
+  possible_outcomes <- c("heart attack", "heart failure", "pneumonia") 
+  if (! outcome %in% possible_outcomes) {
+    print("outcome invalido")
+  } 
+      ## Outcome recebido é valido
+      ## Estado recebido é valido
+      print("calculating ...")
+      outcome_df<-read.csv("outcome-of-care-measures.csv")[,c(1,2,6,7,11,17,23)]
+      # 1=Provider Number
+      # 2=Hospital Name
+      # 6=City
+      # 7=State
+      # 11=Hospital 30-Day Death (Mortality) Rates from Heart Attack
+      # 17=Hospital 30-Day Death (Mortality) Rates from Heart Failure
+      # 23=Hospital 30-Day Death (Mortality) Rates from Pneumonia
+      if (outcome == "heart attack") {
+        filtro <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+      } else if (outcome == "heart failure") {
+        filtro <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+      } else {
+        filtro <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+      }
+      cat(paste("filtro = ", filtro, "\n"))
+      
+      outcome_por_estado <- outcome_df
+      outcome_por_estado$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack <- as.numeric(as.character(outcome_por_estado$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack))
+      outcome_por_estado$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure <- as.numeric(as.character(outcome_por_estado$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure))
+      outcome_por_estado$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia <- as.numeric(as.character(outcome_por_estado$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia))
+      
+      
+      if (num == "worst") {
+        outcome_por_estado <- outcome_por_estado[with(outcome_por_estado, order(-outcome_por_estado[filtro],outcome_por_estado$Hospital.Name)),]
+      } else {
+        cat(paste("ordenação=",num,"\n"))
+        outcome_por_estado <- outcome_por_estado[with(outcome_por_estado, order(outcome_por_estado[filtro],outcome_por_estado$Hospital.Name)),]
+      }
+      
+      us_states_df <- read.csv("state_table.csv")
+      us_states <- as.vector(us_states_df[,3])
+      
+      if (num %in% c("best","worst")) {
+        num <- 1
+      }
+      
+      for (estado in us_states) {
+        outcome_deste_estado <- subset(outcome_por_estado, State==estado)
+        cat(paste(estado, filtro, outcome_deste_estado[num,2],"\n"))
+      }
+      
+      
+      #outcome_por_estado <- outcome_por_estado[with(outcome_por_estado, order(outcome_por_estado[filtro],outcome_por_estado$Hospital.Name)),]
+      
+      
+      cat(paste("ESTADO =", state, "   OUTCOME =", outcome, "   RESPOSTA =", outcome_por_estado[1,2]  ,"\n"))
+      
+    
+    
+  }
+
 
 
 
